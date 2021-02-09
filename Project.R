c2e8b94597155ed1424cb821034343f57c56f554
aaronmorjaria <78754111+aaronmorjaria@users.noreply.github.com>
2021-02-09 20:27:53 +0000

    Update Project.R

--- Project.R
+++ Project.R
@@ -1,1 +1,9 @@
-library(tidyverse)
+#these packages are used for text analysis
+install.packages('tidyverse', repos='http://cran.rstudio.com/') #useful R packages for data science
+install.packages('spacyr', repos='http://cran.rstudio.com/') #used to extract information from texts
+install.packages('readtext', repos='http://cran.rstudio.com/') #used for importing and handling text files (pdf, html and docx are compatible)
+install.packages('quanteda', repos='http://cran.rstudio.com/') #R package for the Quantitative Analysis of Textual Data
+library("tidyverse")
+library("spacyr")
+library("readtext")
+library("quanteda")
