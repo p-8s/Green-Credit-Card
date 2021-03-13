d1cb1a0503251c43aa41a0cf0bdb78b5f396a68b
p-8s <hivresgit@gmail.com>
2021-03-13 16:50:52 +0800

    update profitability model

--- scripts/Project.R
+++ scripts/Project.R
@@ -1,9 +1,15 @@
-#these packages are used for text analysis
-install.packages('tidyverse', repos='http://cran.rstudio.com/') #useful R packages for data science
-install.packages('spacyr', repos='http://cran.rstudio.com/') #used to extract information from texts
-install.packages('readtext', repos='http://cran.rstudio.com/') #used for importing and handling text files (pdf, html and docx are compatible)
-install.packages('quanteda', repos='http://cran.rstudio.com/') #R package for the Quantitative Analysis of Textual Data
-library("tidyverse")
-library("spacyr")
-library("readtext")
-library("quanteda")
+# rough work
+# take_fn <- function (r, p ) {
+#   3 - 10*r - 2*p
+# } #assumption borrowed from So et al
+# 
+# prof_ptf <- function (r, P) {
+#   e_list = c()
+#   p_opt <- ((1-m) * (1+r_F)^N / (l_D * (1+r)^(N-1)) + (l_D-1)/l_D)^(1/N)
+#   integrand <- function(p) {ind_exp_prof(r, p, P) * take_fn(r, p) * (4*p - 2)}
+#   ans = integrate(integrand, lower=p_opt, upper=1)
+#   return(ans)
+# }
+# 
+# 
+# prof_ptf(r=r0, P=590)
